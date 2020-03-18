bd99a41017229f340c97ec0232b0850f5b817ec2
Tulio Gabriel <tullio_cba@hotmail.com>
2020-03-18 09:32:44 -0300

    Estilizando a página main

--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -1,5 +1,49 @@
 import styled from 'styled-components';
 
-export const Title = styled.h1`
-    color: #fff;
+export const Container = styled.div`
+    max-width: 700px;
+    background-color: #fff;
+    border-radius: 4px;
+    padding: 30px;
+    margin: 80px auto;
+    box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.1);
+
+    h1 {
+        font-size: 20px;
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+
+        svg {
+            margin-right: 10px;
+        }
+    }
+`;
+
+export const Form = styled.form`
+    margin-top: 30px;
+    display: flex;
+    flex-direction: row;
+
+    input {
+        flex: 1;
+        border: 1px solid #eee;
+        padding: 10px 15px;
+        border-radius: 4px;
+        font-size: 16px;
+    }
+`;
+
+export const SubmitButton = styled.button.attrs({
+    type: 'submit',
+})`
+    background-color: #7159c1;
+    border: 0;
+    padding: 0 15px;
+    border-radius: 4px;
+    margin-left: 10px;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
 `;
